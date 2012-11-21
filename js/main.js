6c4b40338e5f86c836196a2ae6e7f5b05e8323a8
romanyanke <ppomah@gmail.com>
2012-11-21 10:43:42 -0500

    two random bg's

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+$(function(){
+    var Siberia = [
+        'когда я открывал браузер, на улице ещё было лето',
+        'ещё мой дед делал сайты в Сибири, чтобы не замерзнуть'
+    ];
+    var luck = Math.floor(Math.random()*Siberia.length);
+    $('body').addClass('siberia'+luck);
+    $('h1').html(Siberia[luck]);
+});
