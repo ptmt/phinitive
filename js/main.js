53dc3e6983a964cf67d395fa7af773d281c16cc1
romanyanke <ppomah@gmail.com>
2012-11-21 11:02:29 -0500

    bg hash search #0

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ $(function(){
         'ещё мой дед делал сайты в Сибири, чтобы не замерзнуть'
     ];
     var luck = Math.floor(Math.random()*Siberia.length);
+    if(window.location.hash &&  /#\d{1,2}/.test(window.location.hash)){    
+       var destiny = +window.location.hash.replace('#','')
+       if(destiny <= Siberia.length){
+           luck = destiny;
+       }
+    }
     $('body').addClass('siberia'+luck);
     $('h1').html(Siberia[luck]);
 });
